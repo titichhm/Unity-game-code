2ff676a4c1e9a1fbcbd7c6c49f7507dbfad66f24
Titichh Mishra <48884409+titichhm@users.noreply.github.com>
2020-11-01 04:09:24 +0530

    Changes

---
+++ UNITYprojects/First/Assets/Scripts/Character.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Character : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float moveSpeed;
+    public float flapHeight;
+    public GameObject pipe_up;
+    public GameObject pipe_down;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, flapHeight);
+        }
+
+        if (transform.position.y > 18 || transform.position.y < -19)
+        {
+            Death();
+        }
+
+    }
+
+    public void Death()
+    {
+        rb.velocity = Vector3.zero;
+        transform.position = new Vector2(0, 0);
+    }
+
+}
